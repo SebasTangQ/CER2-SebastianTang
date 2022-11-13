8caf9f34aa7e6f8518ded74e1de5794dcfc116bc
Sebastian Tang <sebastian.tang@sansano.usm.cl>
2022-11-13 19:29:36 -0300

    models update

--- AppResidencia/administracion/admin.py
+++ AppResidencia/administracion/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-    
+
 
 @admin.register(Residencia)
 class RequestResidencia(admin.ModelAdmin):
     model = Residencia
     list_display = ['id','get_residencia','get_propietario','get_fono']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('id', 'calle', 'numero', 'fono', 'propietario')
+        return self.readonly_fields
 
     def get_residencia(self, obj):
         return obj.calle.upper() + ' N°'+obj.numero.upper()
@@ -22,6 +27,11 @@ class RequestResidencia(admin.ModelAdmin):
 class RequestCorrespondencia(admin.ModelAdmin):
     model = Correspondencia
     list_display = ['id','get_residencia']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('id', 'residencia', 'tipo', 'fecha_recepcion')
+        return self.readonly_fields
 
     def get_residencia(self, obj):
         return obj.residencia.calle.upper() + ' N°'+obj.residencia.numero.upper()
